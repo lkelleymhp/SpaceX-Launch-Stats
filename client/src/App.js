49da668363715a3f9488af6aaf58cdfdee2414dd
Loren Kelley <loren.kelley@mhp.com>
2019-04-29 23:48:19 -0400

    Launches/LaunchItem component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import './App.css';
+import Launches from './components/Launches'
+import logo from './logo.png';
 
-function App() {
-  return (
-    <div className="App">
-    <h1>SpaceX </h1>
-    </div>
-  );
+//create client pass in object with 
+const client = new ApolloClient({
+  uri: '/graphql'
+});
+
+class App extends Component {
+  render() {
+    //wrap everything in return with apolloprovider
+    return (
+      <ApolloProvider client={client}>
+      
+          <div className="container">
+            <img
+              src={logo}
+              alt="SpaceX"
+              style={{ width: 300, display: 'block', margin: 'auto' }}
+            />
+           <Launches/>
+          </div>
+
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
